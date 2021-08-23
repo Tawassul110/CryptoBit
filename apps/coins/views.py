e8522a69e54cd838c5a29ed2e09bd602aa4e3429
tawassul110 <tawassul02@gmail.com>
2021-08-23 21:21:15 +0500

    hello

--- apps/coins/views.py
+++ apps/coins/views.py
@@ -6,3 +6,14 @@ def get_coins():
     
     coins = Coin.objects.all()
     return coins
+
+def get_featured_coins():
+    featured_coins = Coin.objects.all()[:3]
+    return featured_coins
+
+def coinDetail(request,coin_id):
+    coin = Coin.objects.get(id = coin_id)  
+    coindata = {
+        'coin' : coin
+     }   
+    return render(request,'pages/details.html', coindata)
